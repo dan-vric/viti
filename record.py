10f9d4fa444920f49bd186e0db8cfde8f60a3902
Dan Bath <daniel.bath@vinelandresearch.com>
2021-11-09 14:19:12 -0500

    connect to network

--- record.py
+++ record.py
@@ -1,12 +1,16 @@
+import sys, os
+print(os.path.dirname(sys.executable))
 from picamera import PiCamera
 from time import sleep
+import datetime
 
 camera = PiCamera()
-
-camera.start_preview(alpha=160)
+TIMESTAMP = datetime.datetime.strftime(datetime.datetime.now(), "%Y%m%d_%H%M%S")
+camera.start_preview(alpha=0)
 camera.exposure_mode='auto'
 camera.awb_mode='auto'
 #camera.annotate_text = "Hello world!"
 sleep(5)
-camera.capture('/home/pi/Desktop/image.jpg')
+camera.capture('/home/pi/Desktop/' + TIMESTAMP + '.jpg')
 camera.stop_preview()
+print("job done")
