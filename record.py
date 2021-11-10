de7c43df9c5c8b2c7e50757f708ecbd5452aa787
Dan Bath <daniel.bath@vinelandresearch.com>
2021-11-10 09:55:22 -0500

    added hostname specific filenames

--- record.py
+++ record.py
@@ -1,16 +1,18 @@
 import sys, os
-print(os.path.dirname(sys.executable))
 from picamera import PiCamera
 from time import sleep
 import datetime
+import socket
+
+TIMESTAMP = datetime.datetime.strftime(datetime.datetime.now(), "%Y%m%d_%H%M%S")
+HOST = socket.gethostname()
 
 camera = PiCamera()
-TIMESTAMP = datetime.datetime.strftime(datetime.datetime.now(), "%Y%m%d_%H%M%S")
 camera.start_preview(alpha=0)
 camera.exposure_mode='auto'
 camera.awb_mode='auto'
 #camera.annotate_text = "Hello world!"
 sleep(5)
-camera.capture('/home/pi/Desktop/' + TIMESTAMP + '.jpg')
+camera.capture('/home/pi/DATA/' + HOST + '-' + TIMESTAMP + '.jpg')
 camera.stop_preview()
 print("job done")
